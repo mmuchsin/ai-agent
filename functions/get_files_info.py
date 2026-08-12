848f73c666d1629f4693ba379c6983d5aae293ea
mmuchsin <kangucin21@gmail.com>
2026-08-12 14:01:05 +0700

    feat: Add function schemas and implement call_function for tool execution

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -33,3 +33,21 @@ def describe_directory(dir_path):
 
     # join lines together and return
     return "\n".join(lines)
+
+
+schema_get_files_info = {
+    "type": "function",
+    "function": {
+        "name": "get_files_info",
+        "description": "Lists files in a specified directory relative to the working directory, providing file size and directory status",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "directory": {
+                    "type": "string",
+                    "description": "Directory path to list files from, relative to the working directory (default is the working directory itself)",
+                },
+            },
+        },
+    },
+}
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from call_function import available_functions, call_function
+from prompts import system_prompt
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="AI Code Assistant")
@@ -21,6 +24,7 @@ def main() -> None:
         api_key=api_key,
     )
     messages = [
+        {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
     if args.verbose:
@@ -33,6 +37,7 @@ def generate_content(client: OpenAI, messages: list, verbose: bool) -> None:
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=messages,
+        tools=available_functions,
     )
     if not response.usage:
         raise RuntimeError("API response appears to be malformed")
@@ -40,9 +45,20 @@ def generate_content(client: OpenAI, messages: list, verbose: bool) -> None:
     if verbose:
         print("Prompt tokens:", response.usage.prompt_tokens)
         print("Response tokens:", response.usage.completion_tokens)
-    print("Response:")
-    print(response.choices[0].message.content)
 
+    print("Response:")
+    message = response.choices[0].message
+
+    if not message.tool_calls:
+        print(message.content)
+        return
+
+    for tool_call in message.tool_calls:
+        result_message = call_function(tool_call, verbose)
+        if not result_message["content"]:
+            raise Exception(f"Error: {result_message['content']}")
+        if verbose:
+            print(f"-> {result_message['content']}")
 
 if __name__ == "__main__":
     main()
